9efbe86dedc7ab5afc5728bcfbd2cddfbbead7aa
altaj1 <altaj1019@gmail.com>
2024-06-01 23:36:46 +0600

    creator dashboard done

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
-// import Sidebar from '../components/Dashboard/Sidebar/Sidebar'
+import Sidebar from '../components/Sidebar/Sidebar'
 
 const DashboardLayout = () => {
   return (
     <div className='relative min-h-screen md:flex'>
       {/* Sidebar */}
-      {/* <Sidebar /> */}
+      <Sidebar />
 
       {/* Outlet --> Dynamic content */}
       <div className='flex-1 md:ml-64'>
